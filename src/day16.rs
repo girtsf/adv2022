78ee516804eb64aa24dfaf38079ff2534f0a004c
Girts Folkmanis <girtsf@users.noreply.github.com>
2022-12-19 22:19:07 -0800

    day 16, part 2 works, but very cheezed (~10GB of RAM and ~10 minutes of runtime)

--- src/day16.rs
+++ src/day16.rs
@@ -1,19 +1,20 @@
 mod lib;
 
-use std::collections::{BTreeSet, HashMap};
+use std::collections::HashMap;
 
+use itertools::Itertools;
 use lazy_static::lazy_static;
 use regex::Regex;
 
 #[derive(Debug)]
-struct Valve {
+struct ParsedValve {
     name: String,
     flow_rate: usize,
     exits: Vec<String>,
 }
 
-impl Valve {
-    fn parse(line: &str) -> Valve {
+impl ParsedValve {
+    fn parse(line: &str) -> ParsedValve {
         lazy_static! {
             // Valve HH has flow rate=22; tunnel leads to valve GG
             // Valve II has flow rate=0; tunnels lead to valves AA, JJ
@@ -21,7 +22,7 @@ impl Valve {
                 Regex::new(r"^Valve (?P<name>\w+) has flow rate=(?P<flow_rate>\d+); tunnels? leads? to valves? (?P<exits>.*)$").unwrap();
         }
         let c = RE.captures(line).unwrap();
-        Valve {
+        ParsedValve {
             name: c.name("name").unwrap().as_str().to_owned(),
             flow_rate: c.name("flow_rate").unwrap().as_str().parse().unwrap(),
             exits: c
@@ -36,64 +37,109 @@ impl Valve {
 }
 
 #[derive(Debug)]
+struct Valve {
+    flow_rate: usize,
+    exits: Vec<u8>,
+}
+
+#[derive(Debug)]
 struct Cave {
-    valves: HashMap<String, Valve>,
+    valves: Vec<Valve>,
+    starting_location: u8,
+    max_pressure_per_minute: usize,
+    pressure_per_minute_per_valve_state: HashMap<u64, usize>,
 }
 
 impl Cave {
     fn parse(input: &str) -> Cave {
-        let valves = input
+        let parsed_valves = input
             .lines()
-            .map(|line| {
-                let valve = Valve::parse(line);
-                (valve.name.clone(), valve)
+            .map(|line| ParsedValve::parse(line))
+            .collect_vec();
+        // Build a mapping from strings to ints.
+        let mapping: HashMap<String, usize> = parsed_valves
+            .iter()
+            .enumerate()
+            .map(|(i, pvalve)| (pvalve.name.clone(), i))
+            .collect();
+        let valves = parsed_valves
+            .iter()
+            .map(|pvalve| Valve {
+                flow_rate: pvalve.flow_rate,
+                exits: pvalve
+                    .exits
+                    .iter()
+                    .map(|ex| *mapping.get(ex).unwrap() as u8)
+                    .collect(),
             })
             .collect();
-        Cave { valves }
+        Cave {
+            valves,
+            starting_location: *mapping.get("AA").unwrap() as u8,
+            max_pressure_per_minute: parsed_valves.iter().map(|pvalve| pvalve.flow_rate).sum(),
+            pressure_per_minute_per_valve_state: HashMap::new(),
+        }
+    }
+
+    fn get_pressure_per_minute(&mut self, valves_open: u64) -> usize {
+        *self
+            .pressure_per_minute_per_valve_state
+            .entry(valves_open)
+            .or_insert_with(|| {
+                self.valves
+                    .iter()
+                    .enumerate()
+                    .map(|(i, valve)| {
+                        if (1 << i) & valves_open != 0 {
+                            valve.flow_rate
+                        } else {
+                            0
+                        }
+                    })
+                    .sum()
+            })
     }
 }
 
 #[derive(Debug, Hash, PartialEq, Eq, Clone)]
 struct State {
-    location: String,
-    // Only for part 2.
-    elephant_location: Option<String>,
-    valves_open: BTreeSet<String>,
-    pressure_per_minute: usize,
+    location: u8,
+    // Only used for part 2.
+    elephant_location: u8,
+    // Bitfield of which valves are open.
+    valves_open: u64,
 }
 
 impl State {
-    fn new(location: &str, elephant: bool) -> State {
+    fn new(location: u8) -> State {
         State {
-            location: location.to_owned(),
-            elephant_location: if elephant {
-                Some(location.to_owned())
-            } else {
-                None
-            },
-            valves_open: BTreeSet::new(),
-            pressure_per_minute: 0,
+            location,
+            elephant_location: location,
+            valves_open: 0,
+            // pressure_per_minute: 0,
         }
     }
     /// Creates a new state that has current valve opened.
-    fn open_valve(&self, loc: &str, rate: usize) -> State {
-        let mut new_state = self.clone();
-        assert!(new_state.valves_open.insert(loc.to_owned()));
-        new_state.pressure_per_minute += rate;
-        new_state
+    fn open_valve(&self, loc: u8) -> State {
+        let bit = 1u64 << (loc as usize);
+        assert_eq!(self.valves_open & bit, 0);
+        State {
+            valves_open: self.valves_open | bit,
+            // pressure_per_minute: self.pressure_per_minute + rate,
+            ..self.clone()
+        }
     }
     /// Creates a new state with a different location.
-    fn move_to(&self, loc: &str) -> State {
+    fn move_to(&self, location: u8) -> State {
         State {
-            location: loc.to_owned(),
+            location,
             ..self.clone()
         }
     }
     /// Creates a new state with a different location for the elephant.
-    fn move_elephant_to(&self, loc: &str) -> State {
-        assert!(self.elephant_location.is_some());
+    fn move_elephant_to(&self, elephant_location: u8) -> State {
         State {
-            elephant_location: Some(loc.to_owned()),
+            elephant_location,
             ..self.clone()
         }
     }
@@ -111,9 +157,9 @@ impl StateUnion {
         }
     }
 
-    fn new_start(elephant: bool) -> StateUnion {
+    fn new_start(starting_location: u8) -> StateUnion {
         StateUnion {
-            pressure_by_state: HashMap::from_iter([(State::new("AA", elephant), 0)]),
+            pressure_by_state: HashMap::from_iter([(State::new(starting_location), 0)]),
         }
     }
 
@@ -122,41 +168,43 @@ impl StateUnion {
         *v = (*v).max(pressure);
     }
 
-    fn next_minute(&self, cave: &Cave) -> StateUnion {
+    fn next_minute(&self, cave: &mut Cave, use_elephant: bool) -> StateUnion {
         let mut next = StateUnion::new();
         for (state, pressure) in self.pressure_by_state.iter() {
+            // if state.pressure_per_minute == cave.max_pressure_per_minute {
+            //     panic!("we got max pressure!");
+            // }
+
             // dbg!(&state, &pressure);
-            let current_valve = cave.valves.get(&state.location).unwrap();
-            let new_pressure = state.pressure_per_minute + pressure;
+            let new_pressure = cave.get_pressure_per_minute(state.valves_open) + pressure;
+            let current_valve = &cave.valves[state.location as usize];
 
             let mut new_states: Vec<State> = Vec::new();
             // If we are in a location with non-zero-rate valve that hasn't been opened yet, try
             // opening it.
-            if !state.valves_open.contains(&state.location) && current_valve.flow_rate != 0 {
-                new_states.push(state.open_valve(&state.location, current_valve.flow_rate));
+            if current_valve.flow_rate != 0 && (state.valves_open & (1 << state.location) == 0) {
+                new_states.push(state.open_valve(state.location));
             }
             // Or try moving to any of the exits.
             for exit in current_valve.exits.iter() {
-                new_states.push(state.move_to(exit));
+                new_states.push(state.move_to(*exit));
             }
 
             // Part 2 (only if we have an elephant).
-            if let Some(ref elephant_location) = state.elephant_location {
-                let elephant_valve = cave.valves.get(elephant_location).unwrap();
+            if use_elephant {
+                let elephant_valve = &cave.valves[state.elephant_location as usize];
                 let mut new_states2: Vec<State> = Vec::new();
                 for new_state in new_states.iter() {
                     // If elephant is in a location with non-zero-rate valve that hasn't been
                     // opened yet, try opening it.
-                    if !new_state.valves_open.contains(elephant_location)
-                        && elephant_valve.flow_rate != 0
+                    if elephant_valve.flow_rate != 0
+                        && (new_state.valves_open & (1 << state.elephant_location) == 0)
                     {
-                        new_states2.push(
-                            new_state.open_valve(elephant_location, elephant_valve.flow_rate),
-                        );
+                        new_states2.push(new_state.open_valve(state.elephant_location));
                     }
                     // Or try moving the elephant to any of the exits.
                     for exit in elephant_valve.exits.iter() {
-                        new_states2.push(new_state.move_elephant_to(exit));
+                        new_states2.push(new_state.move_elephant_to(*exit));
                     }
                 }
                 new_states = new_states2;
@@ -170,13 +218,12 @@ impl StateUnion {
     }
 }
 
-fn plan(cave: &Cave, minutes: usize, elephant: bool) -> usize {
-    let state_union = StateUnion::new_start(elephant);
+fn plan(cave: &mut Cave, minutes: usize, use_elephant: bool) -> usize {
+    let state_union = StateUnion::new_start(cave.starting_location as u8);
     let mut prev = state_union;
     for i in 0..minutes {
         dbg!(&i, prev.pressure_by_state.len());
-        let next = prev.next_minute(&cave);
-        // dbg!(&i, &next);
+        let next = prev.next_minute(cave, use_elephant);
         prev = next;
     }
     *prev.pressure_by_state.values().max().unwrap()
@@ -184,9 +231,10 @@ fn plan(cave: &Cave, minutes: usize, elephant: bool) -> usize {
 
 fn main() {
     let input = lib::read_input();
-    let cave = Cave::parse(&input);
+    let mut cave = Cave::parse(&input);
+    dbg!(&cave);
     // Part 1:
-    dbg!(plan(&cave, 30, false));
+    dbg!(plan(&mut cave, 30, false));
     // Part 2:
-    dbg!(plan(&cave, 26, true));
+    dbg!(plan(&mut cave, 26, true));
 }
